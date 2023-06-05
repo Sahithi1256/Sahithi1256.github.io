60b33359c4c2f381d6a054b057b8b27bfab752e2
Sahithi1256 <94466932+Sahithi1256@users.noreply.github.com>
2023-06-05 22:26:23 +0530

    did js changes

---
+++ usercard2.js
@@ -0,0 +1,25 @@
+let yougerversionofsahithi = {
+    imgurl: "https://i.pinimg.com/736x/be/32/33/be32339ccdaa8b3f82d0a461be5e3d80.jpg",
+    name: "SAHITHI",
+    Description:"III rd "
+}
+let olderversionofsahithi = {
+    imageurl: "https://e0.pxfuel.com/wallpapers/667/489/desktop-wallpaper-cute-cartoon-girl-cute-girl.jpg",
+    name :"Elder version of SAHITHI",
+    Description: "Achieved 40lakhs package in campus placements"
+}
+let issahithiyoung = true;
+let displayobject;
+let flipdata = function(){
+    if(issahithiyoung == true){
+        displayobject = olderversionofsahithi,
+        issahithiyoung = false;
+    }
+    else{
+        displayobject = yougerversionofsahithi,
+        issahithiyoung = true;
+    }
+    document.getElementById("sahihtiimg").src = displayobject.imgurl;
+    document.getElementById("sahithiname").innerHTML = displayobject.name;
+    document.getElementById("sahithides").innerHTML = displayobject.Description;
+}
