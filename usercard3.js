4d3126e9b23dbc63c770d6e2d91911af1efd3837
Sahithi1256 <94466932+Sahithi1256@users.noreply.github.com>
2023-06-13 22:23:58 +0530

    demo

--- usercard3.js
+++ usercard3.js
@@ -9,9 +9,10 @@ let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
     .then(data=>{
-        displayobject.name = data.results[0].name.first + " " + data.results[0].name.last 
-        displayobject.imageurl = data.results[0].picture.large
-        displayobject.Description = data.results[0].gender
+        displayobject.name = "abcd hello"
+        //displayobject.name = data.results[0].name.first + " " + data.results[0].name.last 
+        // displayobject.imageurl = data.results[0].picture.large
+        // displayobject.Description = data.results[0].gender
         document.getElementById("sahihti-img").src = displayobject.imageurl;
         document.getElementById("sahithiname").innerHTML = displayobject.name;
         document.getElementById("sahithi-des").innerHTML = displayobject.Description;
